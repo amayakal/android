77d4c455852592a93150570864364baf71a1bcae
jiangchuan <jiangchuanheglobal@gmail.com>
2014-09-30 11:02:38 -0400

    modify login layout files

---
+++ gen/R.java.d
@@ -0,0 +1,11 @@
+/Users/jiangchuan/repo/My_Android/WRCA/android/gen/com/WRCA/android/R.java \
+ : /Users/jiangchuan/repo/My_Android/WRCA/android/res/drawable-hdpi/ic_launcher.png \
+/Users/jiangchuan/repo/My_Android/WRCA/android/res/drawable-ldpi/ic_launcher.png \
+/Users/jiangchuan/repo/My_Android/WRCA/android/res/drawable-mdpi/ic_launcher.png \
+/Users/jiangchuan/repo/My_Android/WRCA/android/res/drawable-xhdpi/ic_launcher.png \
+/Users/jiangchuan/repo/My_Android/WRCA/android/res/layout/do_verification_layout.xml \
+/Users/jiangchuan/repo/My_Android/WRCA/android/res/layout/get_verification_layout.xml \
+/Users/jiangchuan/repo/My_Android/WRCA/android/res/layout/login_layout.xml \
+/Users/jiangchuan/repo/My_Android/WRCA/android/res/layout/registration_layout.xml \
+/Users/jiangchuan/repo/My_Android/WRCA/android/res/values/strings.xml \
+/Users/jiangchuan/repo/My_Android/WRCA/android/bin/AndroidManifest.xml \
